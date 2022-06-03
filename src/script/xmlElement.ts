dac432dc3636baff1395032bbd9f6561de315c42
nullday <aseaday@hotmail.com>
2022-06-04 00:54:54 +0800

    Add xmlElement

---
+++ src/script/xmlElement.ts
@@ -0,0 +1,73 @@
+const { XMLParser } = require("fast-xml-parser");
+import * as fs from 'fs';
+const options = {
+    ignoreAttributes: false,
+    attributeNamePrefix: "@@"
+};
+const parser = new XMLParser(options);
+
+export default class XmlElement {
+    private jsObj: object;
+    private identityPath: string;
+    constructor(jsObj?: any, path?: string) {
+        if (jsObj) {
+            this.jsObj = jsObj;
+            this.identityPath = path;
+        }
+    }
+
+    public get name(): string {
+        return Object.keys(this.jsObj)[0];
+    }
+
+    LoadFromFile(xmlFileName: string): void {
+
+        this.jsObj = parser.parse(fs.readFileSync(xmlFileName, "utf8"));
+        this.identityPath = "0";
+    }
+
+    Equals(ele: XmlElement) {
+        return ele.identityPath === this.identityPath;
+    }
+
+    Elements(): XmlElement[] {
+        let elementArray = new Array<XmlElement>();
+        let i = 0;
+        const children = this.jsObj[this.name];
+        for (let key in children) {
+            if (!key.startsWith("@@")) {
+                if (Array.isArray(children[key])) {
+                    for (let j = 0; j < children[key].length; j++) {
+                        elementArray.push(new XmlElement({
+                           [key]: children[key][j]
+                        }, this.identityPath + "/" + i.toString()));
+                        i++;
+                    }
+                } else {
+                    i++;
+                    elementArray.push(new XmlElement({
+                        [key]: children[key]
+                    }, this.identityPath + "/" + i.toString()));
+                }
+            }
+        }
+        return elementArray;
+    }
+
+    *MyDescendants(tags: string[]): Generator<XmlElement, void, void> {
+        var q = new Array<XmlElement>();
+        q.push(this);
+        while (q.length > 0) {
+            let e: XmlElement = q.pop();
+            if (!this.Equals(e)) {
+                yield e;
+            }
+            let eles = e.Elements();
+            eles.forEach(element => {
+                if (tags.indexOf(element.name) >= 0) {
+                    q.push(element);
+                }
+            });
+        }
+    }
+}
--- tests/xmlVendor.spec.ts
+++ tests/xmlVendor.spec.ts
@@ -1,9 +1,25 @@
-import  {XmlVendor} from '../src/script/xmlVendor';
+import XmlElment from '../src/script/xmlElement';
 describe("xmlVendor", () => {
-    test("test if xmlVendor read from file.", () => {
-        const xmlDoc = new XmlVendor("models/basic.xml");
-        console.log(xmlDoc.jsObj);
-      })
+  test("test if xmlElemnt get name", () => {
+    const xmlDoc = new XmlElment();
+    xmlDoc.LoadFromFile("models/xml/basic.xml");
+    expect(xmlDoc.name).toBe("one");
+  });
+  test("test if xmlElemnt get elements", () => {
+    const xmlDoc = new XmlElment();
+    xmlDoc.LoadFromFile("models/xml/Noise.xml");
+    expect(xmlDoc.Elements().length).toBe(3);
+  });
+  test("test if xlm could get descendants", () => {
+    const xmlDoc = new XmlElment();
+    xmlDoc.LoadFromFile("models/xml/Noise.xml");
+    let i = 0;
+    for (const ele of xmlDoc.MyDescendants(["all"])) {
+      expect(ele.name).toBe("all");
+      i++;
+    }
+    expect(i).toBe(2);
+  });
 });
 
-export {};
+export { };
