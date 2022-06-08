99dbaa88d90a3be00340c646a290b06a0744265e
nullday <aseaday@hotmail.com>
2022-06-08 23:24:49 +0800

    Add arrayHelper 2D feature

--- src/utils/arrayHelper.ts
+++ src/utils/arrayHelper.ts
@@ -1,11 +1,33 @@
+declare type AnyFunction = (...args: unknown[]) => unknown
+
+function isFunction<T extends AnyFunction>(value: unknown): value is T {
+    return typeof value === 'function'
+}
 export default class ArrayHelper {
-    public static Array1D (length: number, value: ((arg: number) => T) | boolean): T[] {
-        const result: T[] = [];
-        for (let i = 0; i < length; i++) {
-            if (typeof value !== "function") {
-                result.push(value);
-            } else if (typeof value === "function") {
-                result.push(value(i));
+    public static Array1D<T>(length: number, value: ((arg: number) => T) | T): T[] {
+        const result = new Array<T>(length);
+        if (isFunction(value)) {
+            for (let i = 0; i < length; i++) {
+                result[i] = value(i);
+            }
+        } else {
+            for (let i = 0; i < length; i++) {
+                result[i] = value;
+            }
+        }
+        return result;
+    }
+    public static Array2D<T>(MX: number, MY: number, value: ((arg1: number, arg2: number) => T) | T): T[][] {
+        const result = new Array<T[]>(MX);
+        const isFuncFlag = isFunction(value);
+        for (let x = 0; x < MX; x++) {
+            result[x] = new Array<T>(MY);
+            for (let y = 0; y < MY; y++) {
+                if (isFuncFlag) {
+                    result[x][y] = value(x, y);
+                } else {
+                    result[x][y] = value;
+                }
             }
         }
         return result;
